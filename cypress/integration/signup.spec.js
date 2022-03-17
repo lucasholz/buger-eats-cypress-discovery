50d4d8c60dec88a7d40eda0376d783d5851ce512
Lucas Holz <matteholz@gmail.com>
2022-03-17 20:51:20 -0300

    create the Require fild test case

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -44,4 +44,16 @@ describe('Signup', () => {
     signup.submit()
     signup.alertMessageShouldBe('Oops! Email com formato inválido.')
   })
+
+  it('Required field', function () {
+    signup.go()
+    signup.submit()
+    signup.alertMessageShouldBe('É necessário informar o nome')
+    signup.alertMessageShouldBe('É necessário informar o CPF')
+    signup.alertMessageShouldBe('É necessário informar o email')
+    signup.alertMessageShouldBe('É necessário informar o CEP')
+    signup.alertMessageShouldBe('É necessário informar o número do endereço')
+    signup.alertMessageShouldBe('Selecione o método de entrega')
+    signup.alertMessageShouldBe('Adicione uma foto da sua CNH')
+  })
 })
